7e0ef34acc02b5d986d994272d3792db97e8b66a
Georgii Zennurov <32030646+GeorgiiZ@users.noreply.github.com>
2024-02-21 20:50:49 +0300

    Поправил работу с ConfigService (#5)
    * Настроил корректное подключение ConfigService. Поправил подключение к бд при локальной разработке
    
    * Поправил конфликты между запуском через docker и npm
    
    * Поправил конфликты между запуском dev сборок docker и nest
    
    * Вынес получение конфига в общие утилиты
    
    * Убрал хук useConfigService
    
    ---------
    
    Co-authored-by: gzennurov <zennurov.g.r@sblogistica.ru>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeormModule } from './typeorm/typeorm.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeormModule, UserModule],
+  imports: [TypeormModule, UserModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
